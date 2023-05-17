a56f7e3b12a17bf7b675a55c9ce9ae856fe740dc
wyw <740484522@qq.com>
2023-05-17 16:06:42 +0800

    更新侧边栏

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -2,13 +2,13 @@
  ******************** 指南 ********************
  */
 export function getSideBarGuide() {
-    return [
-        {
-            text:'指南',
-            collapsible: true,
-            items: [],
-        },
-    ]
+  return [
+    {
+      text: "指南",
+      collapsible: true,
+      items: [],
+    },
+  ];
 }
 
 /**
@@ -16,62 +16,58 @@ export function getSideBarGuide() {
  */
 // 编辑器相关笔记
 export function getSideBarNoteEditor() {
-    return [
-        {
-            text:'编辑器/IDE',
-            collapsible: true,
-            items: [
-                { text:'VSCode', link:'/note/editor/VSCode.md' },
-            ],
-        },
-    ]
+  return [
+    {
+      text: "编辑器/IDE",
+      collapsible: true,
+      items: [{ text: "VSCode", link: "/note/editor/VSCode.md" }],
+    },
+  ];
 }
 
 // 前端技术栈相关笔记
 export function getSideBarNoteFrame() {
-    return [
-        {
-            text:'前端技术栈',
-            collapsible: true,
-            items: [
-                { text:'Element+', link:'/note/frame/Element.md' },
-                { text:'Uni-app', link:'/note/frame/Uni-app.md' },
-                { text:'VUE', link:'/note/frame/VUE.md' },
-                { text:'GIT', link:'/note/frame/GIT.md' },
-                { text:'JavaScript', link:'/note/frame/JavaScript.md' },
-                { text:'TypeScript', link:'/note/frame/TypeScript.md' },
-            ],
-        },
-    ]
+  return [
+    {
+      text: "前端技术栈",
+      collapsible: true,
+      items: [
+        { text: "GIT", link: "/note/frame/GIT.md" },
+        { text: "JavaScript", link: "/note/frame/JavaScript.md" },
+        { text: "TypeScript", link: "/note/frame/TypeScript.md" },
+      ],
+    },
+  ];
 }
 
 // 配置部署相关笔记
 export function getSideBarNoteDeploy() {
-    return [
-        {
-            text:'配置部署',
-            collapsible: true,
-            items: [
-                { text: 'VitePress', link: '/note/deploy/VitePress.md' },
-                { text: 'Verdaccio', link: '/note/deploy/Verdaccio.md' },
-            ],
-        },
-    ]
+  return [
+    {
+      text: "配置部署",
+      collapsible: true,
+      items: [
+        { text: "Nginx", link: "/note/deploy/Nginx.md" },
+        { text: "VitePress", link: "/note/deploy/VitePress.md" },
+        { text: "Verdaccio", link: "/note/deploy/Verdaccio.md" },
+      ],
+    },
+  ];
 }
 
 // 交流分享笔记
 export function getSideBarNoteShare() {
-    return [
-        {
-            text:'交流分享',
-            collapsible: true,
-            items: [
-                { text: '事件循环', link: '/note/share/事件循环.md' },
-                { text: '文件上传', link: '/note/share/文件上传.md' },
-                { text: 'GitLab-CI', link: '/note/share/GitLab-CI.md' },
-            ],
-        },
-    ]
+  return [
+    {
+      text: "交流分享",
+      collapsible: true,
+      items: [
+        { text: "事件循环", link: "/note/share/事件循环.md" },
+        { text: "文件上传", link: "/note/share/文件上传.md" },
+        { text: "GitLab-CI", link: "/note/share/GitLab-CI.md" },
+      ],
+    },
+  ];
 }
 
 /**
